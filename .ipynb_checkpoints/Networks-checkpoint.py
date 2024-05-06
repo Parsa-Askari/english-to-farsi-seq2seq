ecff7ddf80aabf921ecca76a8ea6a1e11d08efb9
UNCLE-KIZARU <parsaaskari1380@gmail.com>
2024-05-06 23:50:11 +0330

    preprocesss

---
+++ .ipynb_checkpoints/Networks-checkpoint.py
@@ -0,0 +1,10 @@
+import torch.nn as nn
+import torch
+class LSTM(nn.module):
+    def __init__(self):
+        super(LSTM,self).__init__()
+    
+class InputNetwork(nn.Module):
+    def __init__(self):
+        super(InputNetwork,self).__init__()
+        
--- Networks.py
+++ Networks.py
@@ -1,6 +1,10 @@
 import torch.nn as nn
 import torch
+class LSTM(nn.module):
+    def __init__(self):
+        super(LSTM,self).__init__()
+    
 class InputNetwork(nn.Module):
     def __init__(self):
-        super().__init__()
+        super(InputNetwork,self).__init__()
         
